38f5daecc657db43704e30c2b0760c15a23ee4d5
matv864 <matv864@gmail.com>
2022-12-26 09:45:45 +1000

    fix bug in wrapper

--- classes/wrapper.py
+++ classes/wrapper.py
@@ -9,12 +9,37 @@ class FefuMetaDB:
     def __init__(self, device_by_id_url, all_objects_url):
         self.device_by_id_url = device_by_id_url
         self.all_objects_url = all_objects_url
+        self.empty_dict = {
+            "battery": 0, 
+            "co2": 0, 
+            "hum": 0, 
+            "lux": 0, 
+            "noise": 0, 
+            "powertype": 0, 
+            "temp": 0, 
+            "tilt": 0, 
+            "time": "---", 
+            "type": 0
+            }
+
+    def __check_response(self, resp):
+        return (resp == "<Response [200]>")
+
+    def __empty_dict_with_err(self, text):
+        repl = self.empty_dict.copy()
+        repl["time"] = text
+        return repl
 
     def request_objects_list(self) -> dict:
         return self.__validate_json(requests.get(self.all_objects_url).json())
 
     def __request_device_readings(self, device_id: int) -> dict:
-        return self.__validate_json(requests.get(self.device_by_id_url+str(device_id)).json())
+        resp = requests.get(self.device_by_id_url+str(device_id))
+        if self.__check_response(str(resp)):
+            return self.__validate_json(resp.json())
+        else:
+            return self.__empty_dict_with_err(str(resp))
+
 
     def __get_devises_list(self, object_name):
         return self.request_objects_list()[object_name]['devices']
