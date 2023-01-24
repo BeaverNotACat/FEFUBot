473d617239fed288751b09846b4d1aa46d7ea45a
Beaver <beavernotacat@gmail.com>
2023-01-24 18:24:31 +1000

    Refactoring

--- classes/config.py
+++
@@ -1,2 +0,0 @@
-DEVICE_BY_ID_URL='http://metadb.ru/flows/'
-ALL_OBJECTS_URL='http://metadb.ru/objects/get_objects'
--- classes/subscribe.py
+++
@@ -1,3 +0,0 @@
-class Mailing:
-    def find_user(self, id_of_user):
-        return False
